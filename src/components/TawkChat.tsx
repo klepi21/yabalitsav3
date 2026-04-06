1f005bd902a6d449b041e6e21be56efe0d574114
konstantinoslepidas <kolepidas@gmail.com>
2026-04-06 08:02:51 +0300

    hack

--- src/components/TawkChat.tsx
+++ src/components/TawkChat.tsx
@@ -10,7 +10,7 @@ export default function TawkChat() {
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/69d33c5b1678ab1c34d72dcd/1jlgi7i30';
+        s1.src='https://embed.tawk.to/69d33e03b137951c367ff5ff/1jlgikfv3';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
